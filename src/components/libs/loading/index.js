4642089ef9b7ba96529772794ff7bc154ea1bd9e
Yuri Fontella <yurifc4@gmail.com>
2021-08-02 21:58:42 -0300

    loading useState

--- src/components/libs/loading/index.js
+++ src/components/libs/loading/index.js
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react"
+
 const Loading = ({ color }) => {
+
+  const [border, setBorder] = useState()
+
+  useEffect(() => {
+    setBorder(color)
+  }, [color])
 
   return (
     <div className="flex justify-center items-center py-4">
-      <span className={`rounded-full animate-spin duration-300 w-5 h-5 border-t-2 border-${color}`}></span>
+      <span className={`rounded-full animate-spin duration-300 w-5 h-5 border-t-2 border-${border}`}></span>
     </div>
   )
 }
