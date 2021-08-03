e4f107f897541b6726b4798062de5ba1a8793e4a
Yuri Fontella <yurifc4@gmail.com>
2021-08-02 23:03:31 -0300

    finish colors

--- src/components/libs/loading/index.js
+++ src/components/libs/loading/index.js
@@ -1,8 +1,10 @@
 const Loading = ({ color }) => {
 
+  console.log(color)
+
   return (
     <div className="flex justify-center items-center py-4 relative z-50">
-      <div className={`rounded-full animate-spin duration-300 w-5 h-5 border-t-2 loading-${color}`}></div>
+      <div className={`rounded-full animate-spin duration-300 w-5 h-5 border-t-2 ${color === 'blue' ? 'border-blue-700' : color === 'white' ? 'border-white' : 'border-black'}`}></div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   purge: {
     enable: true,
-    layers: ['components'],
     content: ['./src/**/*.{js,jsx}']
   },
   darkMode: 'class', // or 'media' or 'class'
